7d2724ffd7247804606ee16815f5266e89189582
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-08-16 09:43:03 +0200

    solution - in progress

--- cucumber/step_definitions/careerPage.js
+++ cucumber/step_definitions/careerPage.js
@@ -3,3 +3,76 @@
 const { Given, When, Then, setDefaultTimeout } = require('cucumber');
 
 setDefaultTimeout(GLOBAL_TIMEOUT);
+
+
+Given('The career page is opened', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+Then('The logo should be visible', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+Then('The search form should be displayed', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+Then('The keyword input field should be visible', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+Then('The submit button should be visible', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+Then('The location filter box arrow should be visible', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+When('The location filter box is selected', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+Then('The location arrow should be displayed', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+Then('It should provide a way to filter a location by clicking', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+
+Given('The jobs page is opened', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+Then('The logo should be visible', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+Then('The url expected to be the url of the job page', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+ Then('it should contain a job description', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
+
+
+Then('it should contain the apply button', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return 'pending';
+  });
