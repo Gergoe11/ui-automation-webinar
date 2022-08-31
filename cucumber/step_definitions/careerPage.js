1399dd9726ee6650781c03ec76ba177f97482950
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-08-31 17:31:55 +0200

    update

--- cucumber/step_definitions/careerPage.js
+++ cucumber/step_definitions/careerPage.js
@@ -1,4 +1,3 @@
-'use strict';
 'use strict'
 
 const { expect } = require('chai');
@@ -8,6 +7,7 @@ const careerPage = new CareerPage();
 
 
 const { Given, When, Then, setDefaultTimeout } = require('cucumber');
+const { Button } = require('protractor');
 
 setDefaultTimeout(GLOBAL_TIMEOUT);
 
@@ -19,7 +19,7 @@ Given('The career page is opened', function () {
 
 
 Then(/^The "(.+)" should be displayed$/, function (element) {
-    return expect(careerPage[element].isDisplayed()).to.eventually.be.true;;
+    return expect(careerPage[element].isDisplayed()).to.eventually.be.true;
   });
 
 
@@ -40,16 +40,20 @@ Given(/^The "(.+)" is opened$/, function (loadjobs) {
   });
 
 
-Then(/The url expected to be the url of the "jobs page"/, function () {
-    return expect(careerPage.url).to.equal('https://www.epam.com/careers/job-listings?query=debrecen&country=Hungary');
-  });
-
- Then(/it should contain a "job description"/, function () {
-    return expect(careerPage.jobInfo.isDisplayed()).to.eventually.be.true;
+Then(/The url expected to be the url of the "(.+)"/, function (url) {
+    let pageurl = browser.getCurrentUrl();
+    return expect(pageurl).to.eventually.contain(careerPage.url);
   });
 
 
-Then(/it should contain the "apply button"/, function () {
-   const viewJob = careerPage.applyButton.getText()
-   return expect(viewJob).to.eventually.contain('VIEW AND APPLY');
+
+Then(/^It should contain "(.+)"$/, function (element) {
+   return expect(careerPage[element].isDisplayed()).to.eventually.be.true;
   });
+
+  Then(/The "(.+)" should have the text "(.+)"/, function (element,text) {
+    let viewJob = careerPage[element].getText();
+    return expect(viewJob).to.eventually.contain(text);
+   });
+
+
