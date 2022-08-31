0ddfb7a6ffd10f3b1a5737e47f723ac307ddcf0e
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-08-31 14:29:07 +0200

    update

--- pageObjects/careerPage.js
+++ pageObjects/careerPage.js
@@ -9,21 +9,18 @@ const ec = protractor.ExpectedConditions;
 class CareerPage {
     constructor() {
         this.logo = element(by.css('.header__logo'));
-        this.acceptButton = element(by.css('onetrust-accept-btn-handler'));
+        this.acceptButton = element(by.id('onetrust-accept-btn-handler'));
         this.searchForm = element(by.id('new_form_job_search_1445745853_copy-keyword'));
         this.locationArrow = element(by.css('.select2-selection__arrow'));
         //this.country = element(by.xpath('//*[@id="select2-new_form_job_search_1445745853_copy-location-results"]/li[25]/strong'));
-        this.country = element(by.css('li[aria-label="Hungary"] strong'));
-        //this.city = element(by.xpath('//*[@id="jobSearchFilterForm"]/div[2]/div/span/span[1]/span/span[1]'));
+        this.country = element(by.css('li[aria-label="Hungary"] strong'));      
         this.submitButton = element(by.css('.recruiting-search__submit'));
         this.keywordInput = element(by.id('new_form_job_search_1445745853_copy-keyword'));
         this.applyButton = element.all(by.css('.search-result__item-apply'));
         this.jobInfo = element(by.css('.search-result__item-name'));
         this.contactButton = element(by.css('.button__content button__content--desktop'));
-        this.url = 'https://www.epam.com/careers/job-listings?query=debrecen&country=Hungary';
-
-        
-        
+        this.allLocations = element(by.xpath('//*[@id="jobSearchFilterForm"]/div[2]/div/span/span[1]/span/span[1]'));
+                
     }
 
     load() {
@@ -54,8 +51,23 @@ class CareerPage {
       
     }
 
+
+
+    sendKeys(){
+        this.keywordInput.sendKeys();
+        this.submitButton.click();
+        //browser.wait(ec.visibilityOf(this.applyButton), GLOBAL_TIMEOUT)
+    }
+
+   selectAll(){
+        browser.wait(ec.visibilityOf(this.locationArrow), GLOBAL_TIMEOUT);
+        this.locationArrow.click();
+        browser.wait(ec.visibilityOf(this.allLocations), GLOBAL_TIMEOUT);
+        this.allLocations.click();
+
+    }
     
-  
+    
 
 
 
