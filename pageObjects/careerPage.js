6db9844d1709815027ffb3c51a619fe5d2e81182
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-09-02 13:19:28 +0200

    update

--- pageObjects/careerPage.js
+++ pageObjects/careerPage.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { browser } = require("protractor");
+const { browser, protractor } = require("protractor");
 
 const ec = protractor.ExpectedConditions;
 
@@ -11,66 +11,74 @@ class CareerPage {
         this.logo = element(by.css('.header__logo'));
         this.acceptButton = element(by.id('onetrust-accept-btn-handler'));
         this.searchForm = element(by.id('new_form_job_search_1445745853_copy-keyword'));
-        this.locationArrow = element(by.css('.select2-selection__arrow'));
+        this.locationArrow = element(by.xpath('//*[@id="jobSearchFilterForm"]/div[2]/div/span/span[1]/span/span[2]/b'));
         //this.country = element(by.xpath('//*[@id="select2-new_form_job_search_1445745853_copy-location-results"]/li[25]/strong'));
-        this.country = element(by.css('li[aria-label="Hungary"] strong'));      
+        this.country = element(by.css('li[aria-label="Hungary"] strong'));
         this.submitButton = element(by.css('.recruiting-search__submit'));
         this.keywordInput = element(by.id('new_form_job_search_1445745853_copy-keyword'));
+        this.locationInput = element(by.css('.select2-selection__rendered'));
+        this.searchwordInput = element(by.css('.select2-search__field'));
+        this.selectresult = element(by.css('.select2-results__option select2-results__option--highlighted'));
         this.applyButton = element.all(by.css('.search-result__item-apply'));
         this.jobInfo = element(by.css('.search-result__item-name'));
         this.contactButton = element(by.css('.button__content button__content--desktop'));
-        this.allLocations = element(by.xpath('//*[@id="jobSearchFilterForm"]/div[2]/div/span/span[1]/span/span[1]'));
+        //this.allLocations = element(by.xpath('//*[@id="jobSearchFilterForm"]/div[2]/div/span[1]'));
+        this.allLocations = element(by.css('span[title="All Cities in Hungary"]'));
         this.url = 'https://www.epam.com/careers/job-listings?query=debrecen&country=Hungary'
-                
+
     }
 
     load() {
-    browser.get('https://www.epam.com/careers');
-    return browser.wait(ec.elementToBeClickable(this.logo), GLOBAL_TIMEOUT);
+        browser.get('https://www.epam.com/careers');
+        return browser.wait(ec.elementToBeClickable(this.logo), GLOBAL_TIMEOUT);
     };
 
     loadJobs() {
-     
+
         browser.get(this.url);
         return browser.wait(ec.elementToBeClickable(this.logo), GLOBAL_TIMEOUT);
     }
 
     acceptAll() {
         return browser.isElementPresent(this.acceptButton)
-        .then(isPresent => {
-            if(isPresent){
-            this.acceptButton.click();
-            }
-        });
+            .then(isPresent => {
+                if (isPresent) {
+                    this.acceptButton.click();
+                }
+            });
     };
 
     selectLocation() {
-      // browser.wait(ec.elementToBeClickable(this.locationArrow), GLOBAL_TIMEOUT); 
-       this.locationArrow.click();
-       browser.wait(ec.visibilityOf(this.country), GLOBAL_TIMEOUT); 
-       this.country.click();  
-      
+        browser.wait(ec.elementToBeClickable(this.locationArrow), GLOBAL_TIMEOUT); 
+        this.locationArrow.click();
+       
+
+
+    }
+
+    location() {
+        browser.wait(ec.elementToBeClickable(this.allLocations), GLOBAL_TIMEOUT); 
+        this.allLocations.click();
     }
 
 
-
-    sendKeys(){
-        this.keywordInput.sendKeys();
-        this.submitButton.click();
+    sendKeys(city) {
+        //browser.wait(ec.visibilityOf(this.keywordInput), GLOBAL_TIMEOUT)
+        this.keywordInput.sendKeys(city)
+       .sendKeys(protractor.Key.ENTER);
+       // this.submitButton.click();
         //browser.wait(ec.visibilityOf(this.applyButton), GLOBAL_TIMEOUT)
     }
 
-   selectAll(){
-        browser.wait(ec.visibilityOf(this.locationArrow), GLOBAL_TIMEOUT);
-        this.locationArrow.click();
-        browser.wait(ec.visibilityOf(this.allLocations), GLOBAL_TIMEOUT);
-        this.allLocations.click();
-
-    }
-
     
- 
 
+
+
+    writeInput(city) {
+       
+        this.keywordInput.sendKeys(city)
+         .sendKeys(protractor.Key.ENTER)
+    }
 
 
 
