ff737909732092582972b13f78874d1282a13c7c
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-09-09 17:21:39 +0200

    update, step def.file for 2 features

--- cucumber/step_definitions/jobPage.js
+++ cucumber/step_definitions/jobPage.js
@@ -8,6 +8,9 @@ const careerPage = new CareerPage();
 
 const { Given, When, Then, setDefaultTimeout, After, Status } = require('cucumber');
 const { Button, browser } = require('protractor');
+const fs = require('fs');
+const path = require('path');
+
 
 
 setDefaultTimeout(GLOBAL_TIMEOUT);
@@ -16,71 +19,97 @@ setDefaultTimeout(GLOBAL_TIMEOUT);
 
 Given('The career site is opened', function () {
   return careerPage.loadJobs();
-  
-
 });
 
-When(/The (.+) is selected/, function(city) {
-    return careerPage.citySelector();
-
-})
 
 When(/The location arrow is clicked/, function() {
   return careerPage.selectLocation();
 });
-
-When(/The "(.+)" is clicked/, function(element) {
-    return careerPage.clicksearchWord();
-});
-
-When(/The (.+) is picked/, function (city) {
-    return careerPage.selectCity(city);
-});
-
-When(/The location country is clicked/, function () {
-  return careerPage.selectCountry();
-});
-
-When(/The (.+) is chosen/, function (city) {
-  return careerPage.selectCity(city);
-});
-
 
 
 When(/The submit button is clicked/, function() {
   return careerPage.clickSubmit();
 });
 
+
 When(/The (.+) is written into the field/, function (profession) {
   return careerPage.writeKeyword(profession);
 });
+
 
 When(/The (.+) is submitted/, function (city) {
     return careerPage.writeInput(city);
 });
 
+
 When(/The result is clicked/, function () {
    return careerPage.suggestedClick(); 
-  })
+  });
+
 
 Then(/The "(.+)" should be visible/, function (element) {
   return expect(careerPage[element].isDisplayed()).to.eventually.be.true;
 });
 
+
 Then(/The url expected to match with the url of the searched (.+)/, function (job) {
-  let pageUrl = browser.getCurrentUrl();
-  return expect(pageUrl).to.eventually.contain(job);
-
+  let pageurl = browser.getCurrentUrl();
+  return expect(pageurl).to.eventually.contain(job);
 });
 
-Then(/it should include the "(.+)"/, function (element) {
-  browser.wait(ec.visibilityOf(careerPage[element]), GLOBAL_TIMEOUT);
-  return expect(careerPage[element].isDisplayed()).to.eventually.be.true;;
-});
+
 
 Then(/The "(.+)" should contain the text "(.+)"/, function (element, expectedText) {
-  const actualText = careerPage[element].getText();
+  browser.wait(ec.elementToBeClickable(careerPage[element]), GLOBAL_TIMEOUT)
+  let actualText = careerPage[element].getText();
   return expect(actualText).to.eventually.contain(expectedText)
 });
 
 
+Given('The career page is opened', function () {
+  return careerPage.load();
+});
+
+
+Then(/The "(.+)" should be displayed/, function (element) {
+  return expect(careerPage[element].isDisplayed()).to.eventually.be.true;
+});
+
+
+
+Given(/The "(.+)" is opened/, function (loadjobs) {
+  return careerPage.loadJobs();
+});
+
+
+Then(/The url expected to be the url of the "(.+)"/, function (url) {
+  let pageurl = browser.getCurrentUrl();
+  return expect(pageurl).to.eventually.contain(careerPage.url);
+});
+
+
+Then(/It should contain "(.+)"/, function (element) {
+browser.wait(ec.visibilityOf(careerPage[element]), GLOBAL_TIMEOUT)
+ return expect(careerPage[element].isDisplayed()).to.eventually.be.true;
+});
+
+
+Then(/The "(.+)" should have the text "(.+)"/, function (element,text) {
+  let actualText = careerPage[element].getText();
+  return expect(actualText).to.eventually.contain(text);
+ });
+
+
+After(function () {
+  protractor.browser.takeScreenshot().then(function (screenshot) {
+    const screenshots = path.join(process.cwd(), './cucumber');
+
+    fs.writeFile(screenshots + '/test.png', screenshot, 'base64', function (err) {
+      if (err) {
+        throw err;
+      }
+      console.log('File saved.');
+    });
+  });
+});
+  
