faa8d35d9836b6c04263a891cd12f56dd6e7086a
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-09-23 12:36:19 +0200

    finalised

--- pageObjects/careerPage.js
+++ pageObjects/careerPage.js
@@ -1,10 +1,6 @@
 'use strict'
-
 const { browser, protractor } = require("protractor");
-
 const ec = protractor.ExpectedConditions;
-
-
 
 class CareerPage {
     constructor() {
@@ -12,20 +8,13 @@ class CareerPage {
         this.acceptButton = element(by.id('onetrust-accept-btn-handler'));
         this.searchForm = element(by.id('new_form_job_search_1445745853_copy-keyword'));
         this.locationArrow = element(by.css('.select2-selection__arrow'))
-        this.country = element(by.id('select2-new_form_job_search_1445745853_copy-location-result-kftm-all_Hungary'));
         this.submitButton = element(by.css('.recruiting-search__submit'));
         this.keywordInput = element(by.id('new_form_job_search_1445745853_copy-keyword'));
         this.searchwordInput = element(by.css('.select2-search__field'));
-        this.selectresult = element(by.css('.select2-results__option select2-results__option--highlighted'));
         this.applyButton = element(by.css('.search-result__item-apply:first-child'));
-        this.jobInfo = element(by.css('.search-result__item-name'));
-        this.contactButton = element(by.css('.button__content button__content--desktop'));
-        this.allLocations = element(by.css('span[title="All Cities in Hungary"]'));
+        this.jobInfo = element(by.css('.search-result__item-name:first-child'));
         this.url = 'https://www.epam.com/careers/job-listings?country=all'
-        this.allCountries = element.all(by.css('strong.select2-results__group'));
-        this.allCities = element.all(by.css('li.select2-results__option'));
         this.suggestedCity = element(by.css('.select2-results__options.open'));
-                        
     }
 
     load() {
@@ -34,58 +23,33 @@ class CareerPage {
     };
 
     loadJobs() {
-
         browser.get(this.url);
         browser.wait(ec.elementToBeClickable(this.logo), GLOBAL_TIMEOUT);
         return this.acceptAll();
     }
 
     acceptAll() {
-
-              
         return browser.isElementPresent(this.acceptButton)
             .then(isPresent => {
                 if (isPresent) {
                     this.acceptButton.click();
                 }
             });
-
-            
     };
+
+    clickElement(element) {
+        browser.wait(ec.elementToBeClickable(this.element), GLOBAL_TIMEOUT);
+        return this.element.click(element);
+    }
 
     selectLocation() {
         browser.wait(ec.elementToBeClickable(this.locationArrow), GLOBAL_TIMEOUT);
         return this.locationArrow.click();
-       
     }
 
-        
-  
-    clicksearchWord(){
-              browser.wait(ec.elementToBeClickable(this.searchwordInput), GLOBAL_TIMEOUT);
-        return this.searchwordInput.click();
-    }   
-
-    suggestedClick(){
-
-        browser.wait(ec.visibilityOf(this.suggestedCity), GLOBAL_TIMEOUT);
-        return this.suggestedCity.click();
-
-    }
-
-
-        
-    clickSubmit () {
-    browser.wait(ec.elementToBeClickable(this.submitButton), GLOBAL_TIMEOUT);
-    return this.submitButton.click();
-    }
-   
-  
     writeInput(city) {
         browser.wait(ec.visibilityOf(this.searchwordInput), GLOBAL_TIMEOUT);
         return this.searchwordInput.sendKeys(city);
-               
-                
     }
 
     writeKeyword(profession) {
@@ -93,12 +57,7 @@ class CareerPage {
         this.keywordInput.click();
         return this.keywordInput.sendKeys(profession)
     }
-
-
-
 }
-
-
 
 module.exports = CareerPage;
 
